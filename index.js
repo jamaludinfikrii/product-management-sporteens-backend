2487f1274c6effb16a1effa1531e242afaa70e6f
fikri <me@fikri.tech>
2020-09-24 11:24:45 +0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const PORT = 5000
+const mysql = require('mysql')
+
+require('dotenv').config()
+
+const db = mysql.createConnection({
+    user : process.env.DB_USER,
+    password : process.env.DB_PASS,
+    database : process.env.DB_NAME,
+    port : process.env.DB_PORT
+})
+
+
+
+app.get('/' ,(req,res) => {
+    res.send("Welcome")
+})
+
+
+
+
+
+
+
+app.listen(PORT, () => console.log("API RUNNING ON PORT " + PORT))
---
+++ notes.js
@@ -0,0 +1,1 @@
+// install express multer mysql jsonwebtoken dotenv cors
