969fc8dde25c2a2976a711b401e765a507ad571c
fikri <me@fikri.tech>
2020-09-25 10:16:22 +0700

    multer test

--- index.js
+++ index.js
@@ -4,19 +4,22 @@ const PORT = 5000
 const mysql = require('mysql')
 const multer = require('multer')
 
-var storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, 'storage')
-    },
-    filename: function (req, file, cb) {
+// diskStorage
+// Filter
+// file || files
 
-        cb(null, 'PROD' + '-' + Date.now() + '.' +file.mimetype.split('/')[1])
+
+
+var diskStorage = multer.diskStorage({
+    destination : (req,file,next) => {
+        next(null , 'storage')
+    },
+    filename : (req,file,next) => {
+        next(null, 'PIMG-' + Date.now() + '.' + file.mimetype.split('/')[1])
     }
 })
 
-
-const multerMiddleware = multer({storage : storage}).single('image_1')
-
+var testUpload = multer({storage : diskStorage}).single('image')
 
 require('dotenv').config()
 
@@ -28,16 +31,33 @@ const db = mysql.createConnection({
 })
 
 
-
 app.get('/' ,(req,res) => {
-    res.send("Welcome")
+    res.send("<h1>Welcome</h1)")
 })
 
-app.use('/storage', express.static('storage'))
+app.post('/product'  ,(req,res) => {
+    // {name dan price} ==> products
+    // {id_product , images} ==> product_images
+})
 
-app.post('/product' , multerMiddleware ,(req,res) => {
-    const file = req.file
-    
+app.post('/testupload',(req,res) => {
+    testUpload(req,res,(err) => {
+        try {
+            if(err) throw err
+            if(req.file === undefined) throw 'File not found' 
+
+            console.log(req.file)
+            res.send('sukses')
+            
+        } catch (error) {
+            res.json({
+                error : true,
+                message : "Multer Error",
+                detail : error
+            })
+        }
+    })
+
 })
 
 
--- notes.js
+++ notes.js
@@ -1,1 +1,5 @@
 // install express multer mysql jsonwebtoken dotenv cors
+
+// upload file
+    // file nya di API
+    // path nya simpen ke database
