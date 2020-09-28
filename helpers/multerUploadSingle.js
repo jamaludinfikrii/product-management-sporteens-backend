e05c82703a91fd0989f6052ef00f8ec513712b46
fikri <me@fikri.tech>
2020-09-28 10:27:03 +0700

    insert bulk images

---
+++ helpers/multerUploadSingle.js
@@ -0,0 +1,28 @@
+const multer = require('multer')
+const multerUploadSingle = () => {
+
+    var diskStorage = multer.diskStorage({
+        destination : (req,file,next) => {
+            next(null , 'storage')
+        },
+        filename : (req,file,next) => {
+            next(null, 'PIMG-' + Date.now() + '.' + file.mimetype.split('/')[1])
+        }
+    })
+
+    var fileFilter = (req,file,next) => {
+        try {
+            if(file.mimetype.includes('image') === false) throw 'File Must Be An Image'
+            next(null,true)
+        } catch (error) {
+            req.bebas = error
+            next(null,false)
+        }
+    }
+
+    var upload = multer({storage : diskStorage , fileFilter : fileFilter , limits : {fileSize : 200000}}).single('image')
+    return upload
+}
+
+
+module.exports = multerUploadSingle
--- index.js
+++ index.js
@@ -2,33 +2,8 @@ const express = require('express')
 const app = express()
 const PORT = 5000
 const mysql = require('mysql')
+const upload = require('./helpers/multerUploadSingle')()
 const multer = require('multer')
-const { json } = require('express')
-
-// diskStorage
-// Filter
-// file || files
-
-var diskStorage = multer.diskStorage({
-    destination : (req,file,next) => {
-        next(null , 'storage')
-    },
-    filename : (req,file,next) => {
-        next(null, 'PIMG-' + Date.now() + '.' + file.mimetype.split('/')[1])
-    }
-})
-
-var fileFilter = (req,file,next) => {
-    try {
-        if(file.mimetype.includes('image') === false) throw 'File Must Be An Image'
-        next(null,true)
-    } catch (error) {
-        req.bebas = error
-        next(null,false)
-    }
-}
-
-var testUpload = multer({storage : diskStorage , fileFilter : fileFilter , limits : {fileSize : 200000}}).single('image')
 
 require('dotenv').config()
 
@@ -45,7 +20,7 @@ app.get('/' ,(req,res) => {
 })
 
 app.post('/product'  ,(req,res) => {
-    testUpload(req,res,(err) => {
+    upload(req,res,(err) => {
         try {
             if(err) throw err
             
@@ -60,20 +35,18 @@ app.post('/product'  ,(req,res) => {
             // get text data {price and name}
             var data = req.body.data
             try {
-                var dataPased = JSON.parse(data)
+                var dataParsed = JSON.parse(data)
             } catch (error) {
                 console.log(error)
             }
-            console.log(dataPased)
             
             
 
             // insert query data to products
-            db.query('insert into products set ?',data,(err,result) => {
-                const id_product = result.insertId
+            db.query('insert into products set ?',dataParsed,(err,result) => {
                 try {
                     if(err) throw err
-
+                    let id_product = result.insertId
                     // insert path and id_product
                     db.query('insert into product_images set ?',{image : imagePath , id_product},(err,result) => {
                         try {
@@ -108,26 +81,85 @@ app.post('/product'  ,(req,res) => {
 
 })
 
-app.post('/testupload',(req,res) => {
-    testUpload(req,res,(err) => {
+const storage = multer.diskStorage({
+    destination : (req,file,next) => {
+        next(null,'storage')
+    },
+    filename : (req,file,next) => {
+        next(null,'IMG-' + Date.now() + '.' + file.mimetype.split('/')[1])
+    }
+})
+
+const fileFilter = (req,file,next) => {
+    try {
+        if(file.mimetype.includes('image') === false) throw 'File Must Be An Image'
+        next(null,true)
+    } catch (error) {
+        req.multerFilterError = error
+        next(null,false)
+    }
+}
+
+
+const upload2 = multer({storage : storage, fileFilter : fileFilter,limits : {fileSize : 110000}}).array('images',5)
+
+
+
+
+
+app.post('/products-2' , (req,res) => {
+    upload2(req,res,(err) =>{
+        
         try {
             if(err) throw err
-            if(req.fileValidation) throw req.fileValidation
-            if(req.file === undefined) throw 'File not found' 
+            console.log(req.files)
+            let dataParsed = JSON.parse(req.body.data)
+            console.log(dataParsed)
 
-            console.log(req.file)
-            res.send('sukses')
-            
+            db.query('insert into products set ?', dataParsed , (err,result) => {
+                try {
+                    if(err) throw err
+                    let id = result.insertId
+
+                    // generate array of array
+                    let dataToInsert = req.files.map((val) =>{ 
+                        return [
+                            'http://localhost:5000/' + val.path,
+                            id
+                        ]
+                    })
+
+                    console.log(dataToInsert)
+
+                    db.query('insert into product_images (image,id_product) values ?',[dataToInsert],(err,result) => {
+                        try {
+                            if(err) throw err
+                            res.send("Insert data success")
+                        } catch (error) {
+                             res.json({
+                                error : true,
+                                error
+                            })
+                        }
+                    
+                    })
+
+                } catch (error) {
+                    res.json({
+                        error : true,
+                        error
+                    })
+                }
+            })
         } catch (error) {
             res.json({
                 error : true,
-                message : "Multer Error",
-                detail : error
+                error
             })
         }
     })
-
 })
+
 
 
 app.listen(PORT, () => console.log("API RUNNING ON PORT " + PORT))
